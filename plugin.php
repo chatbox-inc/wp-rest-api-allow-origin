fe56289fe5231e370b7156edf40f56d929b4ad63
mikakane <mikakane2@gmail.com>
2016-10-09 14:26:27 +0900

    update

--- plugin.php
+++ plugin.php
@@ -9,15 +9,17 @@ Author URI: (プラグイン作者の URI)
 License: (ライセンス名の「スラッグ」 例: GPL2)
 */
 
-function add_allow_header( $headers ) {
-    global $wp;
+if(function_exists("add_allow_header") && function_exists("add_filter")){
+    function add_allow_header( $headers ) {
+        global $wp;
 
-    $origin = defined("WP_REST_API_ALLOW_ORIGIN")?WP_REST_API_ALLOW_ORIGIN:"*";
+        $origin = defined("WP_REST_API_ALLOW_ORIGIN")?WP_REST_API_ALLOW_ORIGIN:"*";
 
-    if (preg_match ('/wp-json/',$wp->request)) {
-        $headers['Access-Control-Allow-Origin'] = $origin;
-        $headers['Access-Control-Allow-Credentials'] = 'true';
-        return $headers;
+        if (preg_match ('/wp-json/',$wp->request)) {
+            $headers['Access-Control-Allow-Origin'] = $origin;
+            $headers['Access-Control-Allow-Credentials'] = 'true';
+            return $headers;
+        }
     }
+    add_filter( 'wp_headers', 'add_allow_header' );
 }
-add_filter( 'wp_headers', 'add_allow_header' );
